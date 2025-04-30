2b8489f04a713755795e36e4dadc131324496314
ABDUL MANAP <11210195@bsi.ac.id>
2025-04-30 17:11:56 +0700

    Add: Image Featured

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,7 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'PT. Qualita Indonesia';
-export const SITE_DESCRIPTION = 'Qualita Indonesia - Field Engineer Services. Reliable and experienced field technician provider services at 125 points throughout Indonesia.';
+export const SITE_DESCRIPTION = 'PT. Qualita Indonesia is an excellent technical management company in Indonesia. We provide flexibility and cost-effective approaches of the highest quality process, confidentiality, and on-time delivery. Reliable and experienced field technician provider services at 125 points throughout Indonesia.';
 export const SITE_TITLE_CONTACT = 'Contact Us';
 export const SITE_DESCRIPTION_CONTACT = 'Contact Us for technical management services.';
 
