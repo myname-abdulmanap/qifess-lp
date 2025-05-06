4a7eea6ac451dfb609b88a1990da15d92e8417bc
ABDUL MANAP <11210195@bsi.ac.id>
2025-05-06 16:34:24 +0700

    Reelase lp

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'ProtectCube - IoT Based Security Solution';
+export const SITE_TITLE = 'ProtectQube - IoT Based Security Solution';
 export const SITE_DESCRIPTION = 'Welcome to protectQube, Your Reliable IoT-Based Security Solution Ensure the safety of your devices with real-time monitoring and customizable security features. Protect hard-to-reach and widely dispersed equipment with ease. ';
 export const SITE_TITLE_CONTACT = 'Contact Us';
 export const SITE_DESCRIPTION_CONTACT = 'Contact Us for technical management services.';
