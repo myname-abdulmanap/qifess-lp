752141d105e1d07593beb6bc5901e80e8bee0472
ABDUL MANAP <myname.abdulmanap@gmail.com>
2025-11-07 14:02:10 +0700

    update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://protectcube.vercel.app',
+  site: 'https://qifess.com',
   integrations: [mdx(), sitemap()], 
   adapter: vercel(),
   vite: {
